d9ab89083e1931e687834a81bd3e5b21ac744f56
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-21 18:45:52 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home/home_model.dart
+++ lib/pages/home/home_model.dart
@@ -1,4 +1,5 @@
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
 import 'home_widget.dart' show HomeWidget;
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,9 @@ class HomeModel extends FlutterFlowModel<HomeWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // State field(s) for DropDown widget.
+  String? dropDownValue;
+  FormFieldController<String>? dropDownValueController;
 
   /// Initialization and disposal methods.
 
